5de9f149027fd90dd3f1994d397cda2f9d6484a4
lakshmi2244 <golilakshmiprasanna2244@gmail.com>
2022-11-30 12:04:18 +0530

    props example

---
+++ src/App.js
@@ -0,0 +1,27 @@
+/*import ClassComponent from "./ClassComponent"
+import ClassComponent2 from "./ClassComponent2"
+function App() {
+  return (
+      <section>
+        <p>hi</p>
+        <h1>hii</h1>
+        <ClassComponent/>
+        <ClassComponent2/>
+        <img src="https://c1.wallpaperflare.com/preview/858/132/992/beautiful-bird-bright-close-up.jpg" alt="" width="25%"/>
+      </section>
+  )
+}
+
+export default App;
+*/
+
+import PropsExample from "./PropsExample"
+function App(){
+  return(
+      <div>
+        <h1>This is about props examples</h1>
+        <PropsExample name="prasanna" age="20"/>
+      </div>
+    )
+}
+export default App
---
+++ src/ClassComponent2.js
@@ -0,0 +1,11 @@
+import React from "react"
+class ClassComponent2 extends React.Component{
+	render(){
+		return(
+			<div>
+				from ClassComponent2
+			</div>
+		)
+	}
+}
+export default ClassComponent2
