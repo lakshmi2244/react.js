c44b25faca816bc02f06977791711f35a04bb7ae
lakshmi2244 <golilakshmiprasanna2244@gmail.com>
2022-12-07 12:16:26 +0530

    react-redux

---
+++ reduxeg/src/App.js
@@ -0,0 +1,17 @@
+import {useState} from "react"
+import {useSelector,useDispatch} from "react-redux"
+import {increment,decrement} from"./actions"
+
+function App() {
+  const count=useSelector(state=>state.counter)
+  const dispatch=useDispatch()
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={()=>{dispatch(increment())}}>+</button>
+      <button onClick={()=>{dispatch(decrement())}}>-</button>
+      </div>
+  );
+}
+
+export default App;
